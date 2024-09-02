3d60ad6f199df7c4ef2d1906ae09711e0131e8db
Rei Watanabe <164761676+Rei9005@users.noreply.github.com>
2024-09-02 18:42:21 +0900

    QuestionTimer

---
+++ src/components/Header.jsx
@@ -0,0 +1,6 @@
+
+export default function Header() {
+  return <header>
+    <h1>ONE PIECE Quiz</h1>
+  </header>
+}
---
+++ src/components/Quiz.jsx
@@ -0,0 +1,51 @@
+import { useState, useCallback } from "react";
+
+import QUESTIONS from "../questions.js";
+import QuestionTimer from "./QuestionTimer.jsx";
+
+export default function Quiz() {
+  const [userAnswer, setUserAnswer] = useState([]);
+
+  const activeQuestionIndex = userAnswer.length;
+  const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
+
+const handleSelectAnswer = useCallback(function handleSelectAnswer(selectedAnswer) {
+    setUserAnswer((prevUserAnswers) => {
+      return [...prevUserAnswers, selectedAnswer];
+    });
+  }, [])
+
+  const handleSkipAnswer = useCallback(() => handleSelectAnswer(null), [handleSelectAnswer] )
+
+  if (quizIsComplete) {
+    return (
+      <div id="summary">
+        <h2>Quiz Completed!</h2>
+      </div>
+    );
+  }
+  const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
+  shuffledAnswers.sort(() => Math.random() - 0.5);
+
+  return (
+    <div id="quiz">
+      <div id="question">
+        <QuestionTimer
+        key={activeQuestionIndex}
+          timeout={10000}
+          onTimeout={handleSkipAnswer}
+        />
+        <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
+        <ul id="answers">
+          {shuffledAnswers.map((answer) => (
+            <li key={answer} className="answer">
+              <button onClick={() => handleSelectAnswer(answer)}>
+                {answer}
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
